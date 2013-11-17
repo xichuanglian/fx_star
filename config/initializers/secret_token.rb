8b8313718af1baf0b1aaa10ef36b231c1c7ea3e5
Da Wei <xichuanglian@gmail.com>
2013-11-17 22:31:50 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FxStar::Application.config.secret_key_base = '629c1f1f89efccf0e88d999bfea1c519a8b963c981c6ba334d343bffaa7b028db3592ac226d685e39304f936ccaaf5d7cff108691c3996f6bfd78867413a7ac0'
